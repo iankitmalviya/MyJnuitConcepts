64666dad939231699befdb7a709ade1958fa597a
iankitmalviya <iankitmalviya@gmail.com>
2021-10-13 18:25:35 +0530

    First Commit

---
+++ MyJunitConcept/Test/com/ankit/MyAnkitTest.java
@@ -0,0 +1,53 @@
+package com.ankit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyAnkitTest {
+
+	Ankit obj=new Ankit();
+	
+	
+	
+	@Test
+	public void test1() {
+int result=obj.sum(new int[] {1,2,3});
+assertEquals(6,result);
+System.out.println("test1");
+	}
+	
+	//Test Case Going to be passed
+	@Test
+	public void test2() {
+		int result=obj.sum(new int[] {2,3});
+		
+		assertEquals(5,result);
+		System.out.println("test2");
+	}
+	
+	//Test Case Going to be Failed
+	@Test
+	public void test3() {
+		int result=obj.sum(new int[] {1,2,3,3});
+		assertEquals(9,result);
+		System.out.println("test3");
+	}
+	
+	@AfterEach
+	public void test5() {
+		System.out.println("in AfterEach");	}
+
+
+	@BeforeEach
+	public void before() {
+ System.out.println("in BeforeEach");
+	}
+}
---
+++ MyJunitConcept/src/com/ankit/Ankit.java
@@ -0,0 +1,12 @@
+package com.ankit;
+public class Ankit {
+	
+	public int sum(int[] arr) {
+		int sum1=0;
+		for(int i=0;i<arr.length;i++) {
+			sum1+=arr[i];
+		}
+		return sum1;
+	}
+
+}
